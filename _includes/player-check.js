86bbf049db2d214e68f51693d6eec68f5b0a17fd
a-d <alexander_duemont@web.de>
2019-05-04 03:34:25 +0200

    Fix bandcamp body style

--- _includes/player-check.js
+++ _includes/player-check.js
@@ -1,5 +1,7 @@
 this.onload = null;
 contentWindow.document.body.style.cursor='pointer';
+contentWindow.document.body.style.width='100%';
+contentWindow.document.body.style.height='100%';
 contentWindow.document.body.onclick = function() {
     confirm('Klicke auf [OK] um Inhalte von {{ include.name }} interaktiv nachzuladen. Hiermit bestätigst du die Datenschutzbestimmungen dieser Webseite und akzeptierst die Regeln zur Verarbeitung personenbezogener Daten.')
     && (contentWindow.document.location.href='{{ include.url }}')
